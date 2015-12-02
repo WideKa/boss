0b13e31b6462c4a480e2c45c35214398b30c58fa
Jiakun Xu <jiakunxu@gmail.com>
2015-12-02 16:42:57 +0800

    index.vm

--- web/src/main/webapp/statics/js/portal/index.js
+++ web/src/main/webapp/statics/js/portal/index.js
@@ -30,3 +30,13 @@ function submit() {
 	setPassportCookies();
 	window.document.forms[0].submit();
 }
+
+document.onkeydown = function(e) {
+	var theEvent = e || window.event;
+	var code = theEvent.keyCode || theEvent.which || theEvent.charCode;
+	if (code == 13) {
+		submit();
+		return false;
+	}
+	return true;
+}
